306a1147dd5b8fcfce56e7e5fd080de3c20cde4d
Ernest <nowakernest@gmail.com>
2019-12-11 01:32:09 +0100

    Minification
    javascript.js WASNT MINIFICATED yet

--- js/javascript.js
+++ js/javascript.js
@@ -20,3 +20,43 @@ window.addEventListener("scroll", event => {
     }
   });
 });
+
+//Nav mobile
+function myFunction() {
+  let x = document.querySelector("nav ul .nav__links");
+    if (!x.classList.contains("displayNone")){
+        x.classList.add("displayNone");
+    } else {
+        x.classList.remove("displayNone");
+    }
+}
+
+//Change menu from mobile to normal 
+function checkMenu() { 
+ let m = document.getElementById('nav__links');
+ if(window.innerWidth>=768){
+     if(m.classList.contains("displayNone")){
+         m.classList.remove("displayNone");
+     }   
+ } else 
+ {
+    m.classList.add("displayNone");
+ }
+//            m.addEventListener("click", modifyText, false); 
+} 
+
+
+
+//function mobileMenu__collapse(){
+//    alert("It works1!");
+//     if(window.innerWidth<=768){
+//        let x = document.querySelector("nav ul .nav__links");
+//        x.classList.add("displayNone");
+//         alert("It works!");
+//    }
+//}
+//
+//mainNavLinks.forEach(function(el){
+//    el.addEventListener("click", mobileMenu__collapse(), false);
+//}
+//);
